3aa9bd9e45efc12e72b12c8be28f567381fc8b3f
Gabor Vajda <vajdagabor@users.noreply.github.com>
2020-02-08 21:41:49 +0100

    Add React

---
+++ src/UI.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "./index.css"
+
+const App = () => (
+  <>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
