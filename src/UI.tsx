7b97e12bb2c9bbb43e488b59941f24268c842f3d
Gabor Vajda <vajdagabor@users.noreply.github.com>
2020-02-12 13:49:50 +0100

    Add base style

--- src/UI.tsx
+++ src/UI.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import './index.css'
-
+import './styles/base.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
